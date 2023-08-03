71f1bd2ab163476bd798e15e050910797a47695e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-03 20:08:54 +0530

    Add Event On Input Box

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const taskCount = document.getElementById('tasks-counter');
 const taskList = document.getElementById('list');
 
 // Check We Fetch Elements Successfully or Not
+/*
 console.log(taskList)
 console.log(taskCount)
 console.log(add);
-
+*/
 
 
 // Functions
@@ -26,4 +27,28 @@ function checkTask(){}
 function renderList(){}
 
 // Show Notification that task is Added or deleted, Mark Successfully
-function showNotification(massage){}
+function showNotification(massage){
+    window.alert(massage);
+}
+
+
+add.addEventListener('keyup',(e)=>{
+    if(e.key == 'Enter'){
+        let text = e.target.value;
+        
+        // If User Press Enter WithOut Add Any Task
+        if(!text){
+            showNotification('Task Can Not Be Empty')
+            return;
+        }
+        
+        const task = {
+            text,                   //means text : text
+            id : Date.now().toString(),         // Best Way to Give ID
+            done : false
+        }
+        console.log(task.text);
+        e.target.value = '';
+        addTask(task);
+    }
+})
