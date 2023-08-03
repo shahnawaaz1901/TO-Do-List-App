2b90000729f6d00900a98721eda80393cc59fa93
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-03 19:56:05 +0530

    SetUp things for Project

---
+++ script.js
@@ -0,0 +1,29 @@
+const tasks = [];
+
+const add = document.getElementById('add');
+const taskCount = document.getElementById('tasks-counter');
+const taskList = document.getElementById('list');
+
+// Check We Fetch Elements Successfully or Not
+console.log(taskList)
+console.log(taskCount)
+console.log(add);
+
+
+
+// Functions
+
+// Adding a Taks
+function addTask(task){}
+
+// Delete a Task
+function deleteTask(taskId){}
+
+// Mark Task as Complete
+function checkTask(){}
+
+// Refresh Page Every Time After Adding or Delete Task
+function renderList(){}
+
+// Show Notification that task is Added or deleted, Mark Successfully
+function showNotification(massage){}
