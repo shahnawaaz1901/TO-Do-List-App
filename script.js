a45f7593d66ee2a95c94fe9125a902f7c3c16ca7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-03 21:44:43 +0530

    Adding More Functionalities and Render The TO-Do List

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const tasks = [];
+let tasks = [];
 
 const add = document.getElementById('add');
 const taskCount = document.getElementById('tasks-counter');
-const taskList = document.getElementById('list');
+const tasksList = document.getElementById('list');
 
 // Check We Fetch Elements Successfully or Not
 /*
@@ -15,16 +15,61 @@ console.log(add);
 // Functions
 
 // Adding a Taks
-function addTask(task){}
+function addTask(task){
+    if(!task){
+        showNotification('Task Can Not Added');
+        return;
+    }
+    tasks.push(task);
+    showNotification('Task Added Successfully');
+    renderList();
+}
 
 // Delete a Task
-function deleteTask(taskId){}
+function deleteTask(taskId){
+    const new_task = tasks.filter((task)=>{         //Passing Task in Argument Because Search in Only Task Object in the Tasks Array
+        return task.id != taskId;
+    });
+
+    tasks = new_task;
+    showNotification('Task Deleted SuccessFully');
+    renderList();
+}
 
 // Mark Task as Complete
-function checkTask(){}
+function toggleTask(taskId){
+    let selectTask = tasks.filter((task)=>{
+        return task.id == taskId;
+    })
+
+    if(selectTask.length > 0){
+        const curTask = selectTask[0];
+        curTask.done = !curTask.done;
+        showNotification('Task Toggeled Successfully');
+        return;
+    }
+    showNotification('Something went Wrong');
+
+}
 
 // Refresh Page Every Time After Adding or Delete Task
-function renderList(){}
+function addTaskToDOM(task){
+    const li = document.createElement('li');
+    li.innerHTML = 
+    `<input type="checkbox" id="${task.id}" data-id="${task.id}" ${task.done ? 'checked' : ''} class="custom-checkbox">
+    <label for="${task.id}">${task.text}</label>
+    <img src="trash-solid.svg" class="delete" data-id="${task.id}" />`;
+    tasksList.append(li);
+}
+
+
+function renderList(){
+    tasksList.innerHTML = '';
+    for(let i = 0; i < tasks.length;i++){
+        addTaskToDOM(tasks[i]);
+    }
+    taskCount.innerText = tasks.length;
+}
 
 // Show Notification that task is Added or deleted, Mark Successfully
 function showNotification(massage){
@@ -52,3 +97,4 @@ add.addEventListener('keyup',(e)=>{
         addTask(task);
     }
 })
+
